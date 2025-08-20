6d491c1e61957853f0899640f67e36962d61fe96
eetu <eetuhuttula99@gmail.com>
2025-08-20 20:09:07 +0300

    {2] Whole app is in english now

--- add_person.py
+++ add_person.py
@@ -5,9 +5,9 @@ from person import Person
 def add_person():
     """Prompts the user for a new person's details and returns a Person object."""
     try:
-        name = input("Lisää nimi: ")
-        age = int(input("Lisää ikä: "))
-        gender = input("Lisää sukupuoli: ")
+        name = input("Name: ")
+        age = int(input("Age: "))
+        gender = input("Gender: ")
         return Person(name, age, gender)
     except ValueError:
         print("Invalid input. Please enter numbers for age, length, and weight.")

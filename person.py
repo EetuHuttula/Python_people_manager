8beab61bcd73458e5ee7443e37f908d0e0676fab
eetu <eetuhuttula99@gmail.com>
2025-08-20 20:14:14 +0300

    {9} added id

--- person.py
+++ person.py
@@ -1,11 +1,14 @@
 class Person:
+    number_of_people = 0
     """Represents a single person with their attributes."""
     def __init__(self, name, age, gender):
+        Person.number_of_people += 1
+        self.id = Person.number_of_people
         self.name = name
         self.age = age
         self.gender = gender
 
     def __str__(self):
         """Returns a string representation of the person."""
-        return f"Name: {self.name}, Age: {self.age}, Gender: {self.gender}"
+        return f"Id: {self.id}, Name: {self.name}, Age: {self.age}, Gender: {self.gender}"
 

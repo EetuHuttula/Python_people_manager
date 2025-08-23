38a09f43e05a2b623451004444fbdc3da5047401
EetuH <eetuhuttula99@gmail.com>
2025-08-24 00:35:36 +0300

    always capitalize name and gender

--- add_person.py
+++ add_person.py
@@ -5,9 +5,9 @@ from person import Person
 def add_person():
     """Prompts the user for a new person's details and returns a Person object."""
     try:
-        name = input("Name: ")
+        name = input("Name: ").capitalize()
         age = int(input("Age: "))
-        gender = input("Gender: ")
+        gender = input("Gender: ").capitalize()
         return Person(name, age, gender)
     except ValueError:
         print("Invalid input. Please enter numbers for age, length, and weight.")

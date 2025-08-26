79507b8e244fde1c0ad52a597286ee1d559584f7
eetu <eetuhuttula99@gmail.com>
2025-08-26 23:27:17 +0300

    #4 shows counter how many apartments are left

--- add_person.py
+++ add_person.py
@@ -1,9 +1,22 @@
 from person import Person
-from utils.data_utils import is_occupied
+from utils.data_utils import is_occupied, is_every_apartment_full
+from load_people import load_people
 
 def add_person(people_list):
     """Prompts the user for a new person's details and returns a Person object."""
     try:
+        residents = load_people()
+        if is_every_apartment_full(residents):
+            print("All apartments are occupied.")
+            return None
+        elif residents is not None:
+            for i in range(len(residents)):
+                apartments = 18
+                i += 1 
+                left = apartments - i
+                continue
+        print(f"There is still {left} apartments left.")
+
         name = input("Name: ").strip().capitalize()
         age = int(input("Age: "))
         gender = input("Gender: ").strip().capitalize()
@@ -18,6 +31,7 @@ def add_person(people_list):
             print("Invalid floor. Must be 1, 2, or 3.")
             return None
         
+
         # Determine valid apartment numbers based on floor
         valid_apartments = {1: (1, 2, 3), 2: (4, 5, 6), 3: (7, 8, 9)}
         apartment_number = int(input(f"Apartment number ({valid_apartments[floor][0]}-{valid_apartments[floor][-1]}): "))
@@ -25,7 +39,7 @@ def add_person(people_list):
             print(f"Invalid apartment number for floor {floor}.")
             return None
         
-        if is_occupied(people_list, section, floor, apartment_number):
+        if is_occupied(people_list, section, floor, apartment_number, street_address = "rauninkatu 32"):
             print("This apartment is already occupied!")
             return None
         return Person(name, age, gender, section, floor, apartment_number)
